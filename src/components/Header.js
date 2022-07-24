2751953a54f6686c62906667ec944430d694da8c
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-07-24 15:20:47 +0530

    added structure of the header

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+function Header () {
+    return (
+        <div>
+            <div className="header-img">
+                <h1>Siddharth Pande</h1>
+            </div>
+            <div className="about-me">
+                <h2>About me</h2>
+                <p>होगा पत्रिका वार्तालाप विश्व वर्णित दर्शाता सकता बनाने पेदा सदस्य संस्था पहोचाना मुश्किले २४भि असक्षम अंतर्गत सुचना अधिक औषधिक वर्तमान निर्माण अमितकुमार
+                मानसिक एसेएवं तकनिकल दारी मुख्य सारांश वास्तव विभाजनक्षमता जनित बाजार विश्व रहारुप गटकउसि सुना भारत संस्था जिम्मे सीमित रिती दोषसके वास्तव बनाकर खरिदे दिये उपेक्ष लचकनहि वास्तव जिम्मे संपादक अंतर्गत मुखय उन्हे सुना है।अभी भोगोलिक शारिरिक चुनने करके(विशेष संस्क्रुति विस्तरणक्षमता अपनि नवंबर बनाने किएलोग सभिसमज मुख्यतह अथवा जिसकी बदले तकनीकी उपलब्धता
+                </p>
+                <div className="icon-btns">
+                    <div className="github-btn">
+                        <a href="#">
+                            <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                        </a>
+                    </div>
+                    <div className="linkedin-btn">
+                        <a href="#">
+                            <img src="https://bn1303files.storage.live.com/y4mm7RpfKsne6TWQcz82lFCtFAYD7gUAZVZKZF7GFvdLN40VkaNlfVEsPrvZFbuOUXMPKkirintCTmKDXAHTNRMbs-tUMu4myllanoWWF9OX_TbHzxpFUQZJ2lJ2fPeBH66Kywg_uQPchX8ZEKk6K_Rh9KMphaBdw-rk97-xmxgjQmUm1RNLVHgOI1ofYYfm8fu?width=512&height=512&cropmode=none" width="512" height="512" alt="linkedin icon"/>
+                        </a>
+                    </div>
+                    <div className="twitter-btn">
+                        <a href="#">
+                            <img src="https://bn1303files.storage.live.com/y4m8pZ82WRziRzOp4Nj3ebDUqnBVCTXmK8j-EwlofY4X8se2nJoblqGC8fiwYB-CqFLQE9UfouEwPpLUcEmZOBmozEgthjaIhfU34XDNM7y5YjN6_urlATEMXDVFL_bFvhJzT8fCSLkKWy2Gh_InQhtGXW72BJa_cONR6HK7wvUvM4_s78oSu42hxlA6j-ioB5Y?width=512&height=512&cropmode=none" width="512" height="512" alt="twitter icon"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
