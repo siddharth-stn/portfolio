05acf999a68d62c61b26ff892431e099206c08ce
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-08-07 22:10:41 +0530

    Section : main html and styling done

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,13 +7,147 @@ function Main () {
             <div className="workContainer">
                 <div className="workOne">
                     <div className="projectScreenShot">
+                    screenshot of project
                     </div>
                     <div className="projectDetail">
                         <div className="projectHeader">
                             <h3 className="projectName">Project Name</h3>
                             <div className="projectLinks">
-                                {/* Link One */}
-                                {/* Link Two */}
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="projectDescription">
+                            Short description of the project. Just a couple sentences will do. 
+                        </div>
+                    </div>
+                </div>
+                <div className="workTwo">
+                    <div className="projectScreenShot">
+                    screenshot of project
+                    </div>
+                    <div className="projectDetail">
+                        <div className="projectHeader">
+                            <h3 className="projectName">Project Name</h3>
+                            <div className="projectLinks">
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="projectDescription">
+                            Short description of the project. Just a couple sentences will do. 
+                        </div>
+                    </div>
+                </div>
+                <div className="workThree">
+                    <div className="projectScreenShot">
+                    screenshot of project
+                    </div>
+                    <div className="projectDetail">
+                        <div className="projectHeader">
+                            <h3 className="projectName">Project Name</h3>
+                            <div className="projectLinks">
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="projectDescription">
+                            Short description of the project. Just a couple sentences will do. 
+                        </div>
+                    </div>
+                </div>
+                <div className="workFour">
+                    <div className="projectScreenShot">
+                    screenshot of project
+                    </div>
+                    <div className="projectDetail">
+                        <div className="projectHeader">
+                            <h3 className="projectName">Project Name</h3>
+                            <div className="projectLinks">
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="projectDescription">
+                            Short description of the project. Just a couple sentences will do. 
+                        </div>
+                    </div>
+                </div>
+                <div className="workFive">
+                    <div className="projectScreenShot">
+                    screenshot of project
+                    </div>
+                    <div className="projectDetail">
+                        <div className="projectHeader">
+                            <h3 className="projectName">Project Name</h3>
+                            <div className="projectLinks">
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="projectDescription">
+                            Short description of the project. Just a couple sentences will do. 
+                        </div>
+                    </div>
+                </div>
+                <div className="workSix">
+                    <div className="projectScreenShot">
+                    screenshot of project
+                    </div>
+                    <div className="projectDetail">
+                        <div className="projectHeader">
+                            <h3 className="projectName">Project Name</h3>
+                            <div className="projectLinks">
+                                <div className="github-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" width="512" height="512" alt="github icon"/>
+                                    </a>
+                                </div>
+                                <div className="share-btn">
+                                    <a href="#">
+                                        <img src="https://bn1303files.storage.live.com/y4mmhTa8o4frMUSBec-P2w1wXDJkmkH9eH_Zqh5WkrezAQwTOO8ENsdMwmbCvH2c1aUHj71-Z71YL-9p80Nmue1Zv05FzkKKSt7NvGnmaBS5xsyiDnpcKnEtKEBqHcaFHhknYddBRL4U2usVolT_xvH1C5vI4Cro68eSIU7B6Zzf8YNW4El__smR7rHAdNpLjSZ?width=512&height=512&cropmode=none" alt="share icon" />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                         <div className="projectDescription">
