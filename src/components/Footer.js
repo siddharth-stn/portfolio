14e49921f25f2b0cf5c7d3344c71e2d4c875eae3
Siddharth Pande <sidarjunstn.sp@gmail.com>
2022-08-08 22:44:12 +0530

    footer html code and styling almost done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,42 @@ import '../static/footer.scss';
 function Footer () {
     return (
         <div className="footer">
-            
+            <div className="leftSection">
+                <h2>Contact me</h2>
+                <p>Please get in touch if you think our work could be mutually beneficial!</p>
+                <div className="address">
+                    <p>1234 Random Road</p>
+                    <p>Random Town, California 12345</p>
+                </div>
+                <div className="contact">
+                    <div className="phone">
+                        <div className="phoneIcon">
+
+                        </div>
+                        <p>777-109-6066</p>
+                    </div>
+                    <div className="email">
+                        <div className="emailIcon">
+
+                        </div>
+                        <p>sidarjunstn.sp@gmail.com</p>
+                    </div>
+                </div>
+                <div className="iconsGroup">
+                    <div className="githubLink">
+                        <img src="https://bn1303files.storage.live.com/y4ms4i_VT3zEJ9xsRLb3g0W93VHPyDSp2wcxEv428B8LTfEUmzhc1lgF2qWtl-4hBCSqSyOSjtoOL0Ak7r444MCrqSZpuVDScy4QgkJnLz_mdg_wbs3z30AqaaKHU5ec7VauTYQjJnBCwbVPKa34jMLFzrtW_2O0TuBHbx0ERokl48O41qsLLXpsOYoDzcJ4CQ_?width=512&height=512&cropmode=none" alt="github icon" />
+                    </div>
+                    <div className="linkedInLink">
+                        <img src="https://bn1303files.storage.live.com/y4mm7RpfKsne6TWQcz82lFCtFAYD7gUAZVZKZF7GFvdLN40VkaNlfVEsPrvZFbuOUXMPKkirintCTmKDXAHTNRMbs-tUMu4myllanoWWF9OX_TbHzxpFUQZJ2lJ2fPeBH66Kywg_uQPchX8ZEKk6K_Rh9KMphaBdw-rk97-xmxgjQmUm1RNLVHgOI1ofYYfm8fu?width=512&height=512&cropmode=none" alt="linkedIn Icon" />
+                    </div>
+                    <div className="twitterLink">
+                        <img src="https://bn1303files.storage.live.com/y4m8pZ82WRziRzOp4Nj3ebDUqnBVCTXmK8j-EwlofY4X8se2nJoblqGC8fiwYB-CqFLQE9UfouEwPpLUcEmZOBmozEgthjaIhfU34XDNM7y5YjN6_urlATEMXDVFL_bFvhJzT8fCSLkKWy2Gh_InQhtGXW72BJa_cONR6HK7wvUvM4_s78oSu42hxlA6j-ioB5Y?width=512&height=512&cropmode=none" alt="twitter icon" />
+                    </div>
+                </div>
+            </div>
+            <div className="rightSection">
+                <img src="" alt=""/>
+            </div>
         </div>
     );
 }
